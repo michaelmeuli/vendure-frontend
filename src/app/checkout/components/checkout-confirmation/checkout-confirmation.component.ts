55c823e9594b7aff2e97101c7a0979e3a78ed372
Michael Meuli <michael.meuli@gmail.com>
2021-07-01 20:10:18 +0200

    fix creditor address

--- src/app/checkout/components/checkout-confirmation/checkout-confirmation.component.ts
+++ src/app/checkout/components/checkout-confirmation/checkout-confirmation.component.ts
@@ -1,18 +1,17 @@
+import { DOCUMENT } from '@angular/common';
 import {
     ChangeDetectionStrategy,
     ChangeDetectorRef,
     Component,
-    OnInit,
-    AfterViewInit,
-    ViewChild,
     ElementRef,
-    Renderer2,
     Inject,
-} from "@angular/core";
-import { DOCUMENT } from "@angular/common";
-import { ActivatedRoute } from "@angular/router";
-import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
-import { Observable, of } from "rxjs";
+    OnInit,
+    Renderer2,
+    ViewChild,
+} from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
 import {
     filter,
     map,
@@ -20,29 +19,29 @@ import {
     shareReplay,
     switchMap,
     take,
-} from "rxjs/operators";
-import SwissQRBill from "swissqrbill/lib/browser";
+} from 'rxjs/operators';
+import SwissQRBill from 'swissqrbill/lib/browser';
 
-import { REGISTER } from "../../../account/components/register/register.graphql";
-import { GetOrderByCode, Register } from "../../../common/generated-types";
-import { notNullOrUndefined } from "../../../common/utils/not-null-or-undefined";
-import { DataService } from "../../../core/providers/data/data.service";
-import { StateService } from "../../../core/providers/state/state.service";
+import { REGISTER } from '../../../account/components/register/register.graphql';
+import { GetOrderByCode, Register } from '../../../common/generated-types';
+import { notNullOrUndefined } from '../../../common/utils/not-null-or-undefined';
+import { DataService } from '../../../core/providers/data/data.service';
+import { StateService } from '../../../core/providers/state/state.service';
 
-import { GET_ORDER_BY_CODE } from "./checkout-confirmation.graphql";
+import { GET_ORDER_BY_CODE } from './checkout-confirmation.graphql';
 
 @Component({
-    selector: "vsf-checkout-confirmation",
-    templateUrl: "./checkout-confirmation.component.html",
-    styleUrls: ["./checkout-confirmation.component.scss"],
+    selector: 'vsf-checkout-confirmation',
+    templateUrl: './checkout-confirmation.component.html',
+    styleUrls: ['./checkout-confirmation.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckoutConfirmationComponent implements OnInit, AfterViewInit {
+export class CheckoutConfirmationComponent implements OnInit {
     registrationSent = false;
     order$: Observable<GetOrderByCode.OrderByCode>;
     notFound$: Observable<boolean>;
     method: string;
-    @ViewChild("bill", { static: false }) bill: ElementRef;
+    @ViewChild('bill', { static: false }) bill: ElementRef;
     saveQrPdfUrl: SafeResourceUrl;
 
     constructor(
@@ -57,7 +56,7 @@ export class CheckoutConfirmationComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
         const orderRequest$ = this.route.paramMap.pipe(
-            map((paramMap) => paramMap.get("code")),
+            map((paramMap) => paramMap.get('code')),
             filter(notNullOrUndefined),
             switchMap((code) =>
                 this.dataService.query<
@@ -71,32 +70,32 @@ export class CheckoutConfirmationComponent implements OnInit, AfterViewInit {
         this.order$ = orderRequest$.pipe(filter(notNullOrUndefined));
         this.notFound$ = orderRequest$.pipe(map((res) => !res));
         this.route.queryParams.subscribe((params) => {
-            this.method = params["method"];
+            this.method = params['method'];
         });
 
-        if (this.method === "swissqrinvoice") {
+        if (this.method === 'swissqrinvoice') {
             this.order$
                 .pipe(
                     take(1),
                     map((order) => {
                         const data: SwissQRBill.data = {
-                            currency: "CHF",
+                            currency: 'CHF',
                             amount: 1199.95,
-                            reference: "210000000003139471430009017",
+                            additionalInformation: 'ordernummer',
                             creditor: {
-                                name: "Robert Schneider AG",
-                                address: "Rue du Lac 1268",
-                                zip: 2501,
-                                city: "Biel",
-                                account: "CH4431999123000889012",
-                                country: "CH",
+                                name: 'Jessica Meuli',
+                                address: 'Sonnenhaldenstrasse 5',
+                                zip: 8360,
+                                city: 'Wallenwil',
+                                account: 'CH14 0078 1612 4519 5200 2',
+                                country: 'CH',
                             },
                             debtor: {
                                 name: order.customer?.firstName || 'Muster',
-                                address: "Grosse Marktgasse 28",
+                                address: 'Grosse Marktgasse 28',
                                 zip: 9400,
-                                city: "Rorschach",
-                                country: "CH",
+                                city: 'Rorschach',
+                                country: 'CH',
                             },
                         };
                         return data;
@@ -106,34 +105,27 @@ export class CheckoutConfirmationComponent implements OnInit, AfterViewInit {
                     console.log(data);
                     const stream = new (SwissQRBill.BlobStream as any)();
                     const pdf = new SwissQRBill.PDF(data, stream);
-                    pdf.on("finish", () => {
+                    pdf.on('finish', () => {
                         const iframe = document.getElementById(
-                            "iframe"
+                            'iframe'
                         ) as HTMLIFrameElement;
-                        if (iframe) {
-                            iframe.src = stream.toBlobURL("application/pdf");
-                        }
-                        const qrPdfUrl = stream.toBlobURL("application/pdf");
-                        this.saveQrPdfUrl =
-                            this.sanitizer.bypassSecurityTrustResourceUrl(
-                                qrPdfUrl + "#toolbar=0&navpanes=0&scrollbar=1"
-                            );
+                        const qrPdfUrl = stream.toBlobURL('application/pdf');
+                        const optionsQrPdfUrl = qrPdfUrl + '#toolbar=0&navpanes=1&scrollbar=0'
+                        iframe.src = optionsQrPdfUrl;
                         console.log(this.saveQrPdfUrl);
-                        console.log("PDF has been successfully created.");
+                        console.log('PDF has been successfully created.');
                     });
                 });
         }
     }
 
-    ngAfterViewInit() {}
-
     createBillElement(): boolean {
         if (this.saveQrPdfUrl) {
-            const iframe = this.renderer.createElement("iframe");
+            const iframe = this.renderer.createElement('iframe');
             iframe.src = this.saveQrPdfUrl;
-            iframe.width = "100%";
-            iframe.height = "100%";
-            iframe.frameBorder = "0";
+            iframe.width = '100%';
+            iframe.height = '100%';
+            iframe.frameBorder = '0';
             this.renderer.appendChild(this.bill, iframe);
             return true;
         } else {

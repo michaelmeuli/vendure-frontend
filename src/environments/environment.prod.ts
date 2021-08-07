ffc97025b24eec83e1f95c39aa02e0d1cb4802ab
Michael Meuli <michael.meuli@gmail.com>
2021-08-07 13:05:11 +0200

    adjust environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiHost: 'https://demo.vendure.io',
+    apiHost: 'https://vendure.yoga-lichtquelle.ch',
     apiPort: 443,
     shopApiPath: 'shop-api',
     baseHref: '/',

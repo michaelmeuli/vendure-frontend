d004555e0871e143b5016762fd4c395857746b27
Michael Meuli <michael.meuli@gmail.com>
2021-09-15 09:29:41 +0200

    DisableAngular

--- scully.yl-shop-frontend.config.ts
+++ scully.yl-shop-frontend.config.ts
@@ -1,8 +1,16 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+const { DisableAngular } = require('scully-plugin-disable-angular');
 import './scully/plugins/productPlugin';
+
+const postRenderers = [DisableAngular];
+
+setPluginConfig(DisableAngular, 'render', {
+  removeState: true,
+});
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
+  defaultPostRenderers: postRenderers,
   projectName: "yl-shop-frontend",
   outDir: './dist/static',
   routes: {

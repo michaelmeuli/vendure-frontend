77911fac9f7b63f32ef0a325e0971d03d5b76be6
Michael Meuli <michael.meuli@gmail.com>
2021-09-17 20:30:40 +0200

    remove disableAngular Plugin

--- scully.yl-shop-frontend.config.ts
+++ scully.yl-shop-frontend.config.ts
@@ -1,17 +1,10 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
-const { DisableAngular } = require('scully-plugin-disable-angular');
+
 import './scully/plugins/productPlugin';
 
-const postRenderers = [DisableAngular];
-
-setPluginConfig(DisableAngular, 'render', {
-  removeState: true,
-});
-
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  defaultPostRenderers: postRenderers,
-  projectName: "yl-shop-frontend",
+  projectRoot: './src',
+  projectName: 'yl-shop-frontend',
   outDir: './dist/static',
   routes: {
     '/product/:slug': {
@@ -20,18 +13,18 @@ export const config: ScullyConfig = {
   },
   puppeteerLaunchOptions: {
     args: [
-      "--disable-gpu",
-      "--renderer",
-      "--no-sandbox",
-      "--no-service-autorun",
-      "--no-experiments",
-      "--no-default-browser-check",
-      "--disable-dev-shm-usage",
-      "--disable-setuid-sandbox",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process",
-      "--disable-extensions"
+      '--disable-gpu',
+      '--renderer',
+      '--no-sandbox',
+      '--no-service-autorun',
+      '--no-experiments',
+      '--no-default-browser-check',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-extensions'
     ]
   }
 };

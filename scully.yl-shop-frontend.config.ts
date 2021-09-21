1941f00b9bf60c2ef756a218c8e0d126cf4effdc
Michael Meuli <michael.meuli@gmail.com>
2021-09-21 12:05:51 +0200

    add puppeteerLaunchOptions

--- scully.yl-shop-frontend.config.ts
+++ scully.yl-shop-frontend.config.ts
@@ -10,5 +10,21 @@ export const config: ScullyConfig = {
     '/product/:slug': {
         type: 'products',
     }
+  },
+  puppeteerLaunchOptions: {
+    args: [
+      '--disable-gpu',
+      '--renderer',
+      '--no-sandbox',
+      '--no-service-autorun',
+      '--no-experiments',
+      '--no-default-browser-check',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-extensions'
+    ]
   }
 };

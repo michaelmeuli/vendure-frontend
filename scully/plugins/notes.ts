3897fd3b8eeb10de61eb71de4078765e6d1bfd21
Michael Meuli <michael.meuli@gmail.com>
2021-09-29 20:45:28 +0200

    update

---
+++ scully/plugins/notes.ts
@@ -0,0 +1,43 @@
+// https://strapi.io/blog/how-to-build-a-jamstack-app-with-scully-io
+// https://github.com/asotog/scully-strapi-tutorial
+
+import { HandledRoute, registerPlugin } from '@scullyio/scully';
+import { GET_PRODUCTS } from './productPlugin.graphql';
+import {
+    ApolloClient,
+    ApolloLink,
+    concat,
+    gql,
+    HttpLink,
+    InMemoryCache,
+  } from '@apollo/client/core';
+import { environment } from '../../src/environments/environment.prod';
+
+let { apiHost, apiPort, shopApiPath } = environment;
+
+const client = new ApolloClient({
+    link: new HttpLink({
+        uri: `${apiHost}:${apiPort}/${shopApiPath}`
+    }),
+    cache: new InMemoryCache(),
+});
+
+async function productPlugin(
+    route: string,
+    config = {}
+): Promise<HandledRoute[]> {
+    const {
+        data: { products },
+    } = await client.query({
+        query: GET_PRODUCTS,
+    });
+    console.log(products);
+    return Promise.resolve(
+        products.map((slug) => ({
+            route: `/product/${slug}`,
+        }))
+    );
+}
+
+const validator = async (conf) => [];
+registerPlugin('router', 'products', productPlugin, validator);

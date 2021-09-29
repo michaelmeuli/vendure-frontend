04831bad46cd3b8de852191ac343c6ca26320dda
Michael Meuli <michael.meuli@gmail.com>
2021-09-29 20:14:02 +0200

    update

--- scully/plugins/productPlugin.ts
+++ scully/plugins/productPlugin.ts
@@ -1,14 +1,42 @@
-import { HandledRoute, registerPlugin } from '@scullyio/scully'
+// https://strapi.io/blog/how-to-build-a-jamstack-app-with-scully-io
+// https://github.com/asotog/scully-strapi-tutorial
 
-function productPlugin(route: string, config = {}): Promise<HandledRoute[]> {
-  return Promise.resolve([
-    { route: '/product/bergamotte-15ml' },
-    { route: '/product/basilikum-15ml' },
-    { route: '/product/lebensbaum-5ml' },
-    { route: '/product/schwarzer-pfeffer-5ml' },
-    { route: '/product/schwarzfichte-5ml' },
-    { route: '/product/blauer-rainfarn-5ml' },
-  ]);
+import { HandledRoute, registerPlugin } from '@scullyio/scully';
+import { GET_PRODUCTS } from './productPlugin.graphql';
+import {
+    ApolloClient,
+    ApolloLink,
+    concat,
+    gql,
+    HttpLink,
+    InMemoryCache,
+} from '@apollo/client';
+import { environment } from '../../src/environments/environment.prod';
+
+let { apiHost, apiPort, shopApiPath } = environment;
+
+const client = new ApolloClient({
+    link: new HttpLink({
+        uri: `${apiHost}:${apiPort}/${shopApiPath}`
+    }),
+    cache: new InMemoryCache(),
+});
+
+async function productPlugin(
+    route: string,
+    config = {}
+): Promise<HandledRoute[]> {
+    const {
+        data: { products },
+    } = await client.query({
+        query: GET_PRODUCTS,
+    });
+    console.log(products);
+    return Promise.resolve(
+        products.map((slug) => ({
+            route: `/product/${slug}`,
+        }))
+    );
 }
 
 const validator = async (conf) => [];

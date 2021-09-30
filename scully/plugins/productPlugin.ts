77b80196fd38d0b5235e1c9e193c019f046db248
Michael Meuli <michael.meuli@gmail.com>
2021-09-30 09:19:14 +0200

    update

--- scully/plugins/productPlugin.ts
+++ scully/plugins/productPlugin.ts
@@ -1,51 +1,57 @@
-import { HandledRoute, registerPlugin } from '@scullyio/scully'
+import { HandledRoute, registerPlugin } from '@scullyio/scully';
 
 import fetch from 'cross-fetch';
 // Or just: import 'cross-fetch/polyfill';
 
 import {
-  ApolloClient,
-  ApolloLink,
-  concat,
-  gql,
-  HttpLink,
-  InMemoryCache,
+    ApolloClient,
+    ApolloLink,
+    concat,
+    gql,
+    HttpLink,
+    InMemoryCache,
 } from '@apollo/client/core';
 import { environment } from '../../src/environments/environment.prod';
 
 let { apiHost, apiPort, shopApiPath } = environment;
 
 const client = new ApolloClient({
-  link: new HttpLink({
-      uri: `${apiHost}:${apiPort}/${shopApiPath}`,
-      fetch: fetch
-  }),
-  cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: `${apiHost}:${apiPort}/${shopApiPath}`,
+        fetch: fetch,
+    }),
+    cache: new InMemoryCache(),
 });
 
 fetch('//api.github.com/users/lquixada')
-  .then(res => {
-    if (res.status >= 400) {
-      throw new Error("Bad response from server");
-    }
-    return res.json();
-  })
-  .then(user => {
-    console.log(user);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+    .then((res) => {
+        if (res.status >= 400) {
+            throw new Error('Bad response from server');
+        }
+        return res.json();
+    })
+    .then((user) => {
+        console.log(user);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
-function productPlugin(route: string, config = {}): Promise<HandledRoute[]> {
-  return Promise.resolve([
-    { route: '/product/bergamotte-15ml' },
-    { route: '/product/basilikum-15ml' },
-    { route: '/product/lebensbaum-5ml' },
-    { route: '/product/schwarzer-pfeffer-5ml' },
-    { route: '/product/schwarzfichte-5ml' },
-    { route: '/product/blauer-rainfarn-5ml' },
-  ]);
+async function productPlugin(
+    route: string,
+    config = {}
+): Promise<HandledRoute[]> {
+    const {
+        data: { products },
+    } = await client.query({
+        query: GET_PRODUCTS,
+    });
+    console.log(products);
+    return Promise.resolve(
+        products.map((slug) => ({
+            route: `/product/${slug}`,
+        }))
+    );
 }
 
 const validator = async (conf) => [];

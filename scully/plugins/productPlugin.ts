06cca945084111445396cb60c386f726baa11b56
Michael Meuli <michael.meuli@gmail.com>
2021-09-30 08:03:58 +0200

    test node-fetch

--- scully/plugins/productPlugin.ts
+++ scully/plugins/productPlugin.ts
@@ -1,6 +1,16 @@
 import { HandledRoute, registerPlugin } from '@scullyio/scully'
+import fetch from 'node-fetch';
+
+async function test() {
+    const response = await fetch('https://github.com/');
+    const body = await response.text();
+    
+    console.log(body);
+}
+
 
 function productPlugin(route: string, config = {}): Promise<HandledRoute[]> {
+    test();
   return Promise.resolve([
     { route: '/product/bergamotte-15ml' },
     { route: '/product/basilikum-15ml' },

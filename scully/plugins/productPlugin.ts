d0e3ecb4cbb8b0efeecc15d00205665bde71c9e1
Michael Meuli <michael.meuli@gmail.com>
2021-09-30 09:02:16 +0200

    update

--- scully/plugins/productPlugin.ts
+++ scully/plugins/productPlugin.ts
@@ -1,5 +1,22 @@
 import { HandledRoute, registerPlugin } from '@scullyio/scully'
 
+import fetch from 'cross-fetch';
+// Or just: import 'cross-fetch/polyfill';
+
+fetch('//api.github.com/users/lquixada')
+  .then(res => {
+    if (res.status >= 400) {
+      throw new Error("Bad response from server");
+    }
+    return res.json();
+  })
+  .then(user => {
+    console.log(user);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
 function productPlugin(route: string, config = {}): Promise<HandledRoute[]> {
   return Promise.resolve([
     { route: '/product/bergamotte-15ml' },
